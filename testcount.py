f4115295252e7cfada9caa56a680886a0b618cc6
sarvan2004 <163830114+sarvan2004@users.noreply.github.com>
2025-04-14 12:45:26 +0530

    testcount.py

--- testcount.py
+++ testcount.py
@@ -3,65 +3,38 @@ import pandas as pd
 import numpy as np
 from ultralytics import YOLO
 import time
-
 model=YOLO('yolov8s.pt')
-
-
 def RGB(event, x, y, flags, param):
     if event == cv2.EVENT_MOUSEMOVE :  
         colorsBGR = [x, y]
-        print(colorsBGR)
-        
-
+        print(colorsBGR)  
 cv2.namedWindow('RGB')
 cv2.setMouseCallback('RGB', RGB)
-
 cap=cv2.VideoCapture('parking1.mp4')
-
 my_file = open("coco.txt", "r")
 data = my_file.read()
-class_list = data.split("\n")
-   
-
+class_list = data.split("\n")  
 area1=[(52,364),(30,417),(73,412),(88,369)]
-
 area2=[(105,353),(86,428),(137,427),(146,358)]
-
 area3=[(159,354),(150,427),(204,425),(203,353)]
-
 area4=[(217,352),(219,422),(273,418),(261,347)]
-
 area5=[(274,345),(286,417),(338,415),(321,345)]
-
 area6=[(336,343),(357,410),(409,408),(382,340)]
-
 area7=[(396,338),(426,404),(479,399),(439,334)]
-
 area8=[(458,333),(494,397),(543,390),(495,330)]
-
 area9=[(511,327),(557,388),(603,383),(549,324)]
-
 area10=[(564,323),(615,381),(654,372),(596,315)]
-
 area11=[(616,316),(666,369),(703,363),(642,312)]
-
 area12=[(674,311),(730,360),(764,355),(707,308)]
-
-
-
-
 while True:    
     ret,frame = cap.read()
     if not ret:
         break
     time.sleep(1)
     frame=cv2.resize(frame,(1020,500))
-
     results=model.predict(frame)
- 
     a=results[0].boxes.data
     px=pd.DataFrame(a).astype("float")
-
     list1=[]
     list2=[]
     list3=[]
@@ -73,11 +46,8 @@ while True:
     list9=[]
     list10=[]
     list11=[]
-    list12=[]
-    
+    list12=[] 
     for index,row in px.iterrows():
-
- 
         x1=int(row[0])
         y1=int(row[1])
         x2=int(row[2])
@@ -150,9 +120,7 @@ while True:
             if results12>=0:
                cv2.rectangle(frame,(x1,y1),(x2,y2),(0,255,0),2)
                cv2.circle(frame,(cx,cy),3,(0,0,255),-1)
-               list12.append(c)
-              
-            
+               list12.append(c)       
     a1=(len(list1))
     a2=(len(list2))       
     a3=(len(list3))    
@@ -241,12 +209,8 @@ while True:
         cv2.polylines(frame,[np.array(area12,np.int32)],True,(0,255,0),2)
         cv2.putText(frame,str('12'),(752,371),cv2.FONT_HERSHEY_COMPLEX,0.5,(255,255,255),1)
 
-   
-    
     cv2.putText(frame,str(space),(23,30),cv2.FONT_HERSHEY_PLAIN,3,(255,255,255),2)
-
     cv2.imshow("RGB", frame)
-
     if cv2.waitKey(1)&0xFF==27:
         break
 cap.release()
